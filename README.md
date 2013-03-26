75258ca00a4b731b440e0efec1860a3d190417fb
Kailey Lampert <trepmal@gmail.com>
2013-03-25 19:22:12 -0700

    initial release

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Common-Function-Names
 =====================
 
 Functions and Classes used in tutorials and examples. Higher probability of conflict with other plugins.
+
+
+Installed as must-use plugin. Make it easy to find plugins prone to conflict.
---
+++ common-functions.php
@@ -0,0 +1,37 @@
+<?php
+
+class options_page {}
+class My_Widget {}
+class Foo_Widget {}
+class My_Widget_Class {}
+
+
+function my_plugin_menu() {}
+function my_plugin_page() {}
+function mt_add_pages() {}
+function register_custom_menu_page() {}
+function custom_menu_page() {}
+function my_remove_menu_pages() {}
+function register_my_custom_submenu_page() {}
+function my_custom_submenu_page_callback() {}
+function adjust_the_wp_menu() {}
+function example_dashboard_widget_function() {}
+function example_add_dashboard_widgets() {}
+function example_remove_dashboard_widgets() {}
+function get_dashboard_widget_options() {}
+function get_dashboard_widget_option() {}
+function update_dashboard_widget_options() {}
+function foobar_func() {}
+function bartag_func() {}
+function my_shortcode_handler() {}
+function myShortCode() {}
+function caption_shortcode() {}
+function myplugin_activate() {}
+function myplugin_deactivate() {}
+function frosty_flush_rewrite_rules() {}
+function maybe_rewrite_rules() {}
+function my_flush_rules() {}
+function my_insert_rewrite_rules() {}
+function my_insert_query_vars() {}
+function keywords_create_rewrite_rules() {}
+function feed_dir_rewrite() {}
